67e98971e7cb6f9c44fdaa4983c3d2b304aa1743
mark l chaves <mark@caughtmyeye.cc>
2022-03-25 20:31:19 +0800

    First commit

--- README.md
+++ README.md
@@ -4,11 +4,24 @@ plugin-safe-mode
 Enables a debugging safe mode by preventing any plugins from loading.
 
 ## Installation
-add plugin-safe-mode.php to your mu-plugins folder.  If you do not have an mu-plugins folder it must be created first
+Add the `plugin-safe-mode.php` file to your `/wp-content/mu-plugins` folder. Create the `mu-plugins` directory if you don't have one.
 
-To activate safe mode:
+**Note**: Copy only the PHP file not the directory that the file is in.
 
-- add define( 'WP_DISABLE_PLUGINS', true ); to wp-config.php
-- Append the query string ?safe_mode=true to any url and the page will be loaded without any plugins active
+---
 
-See: http://core.trac.wordpress.org/ticket/25137
+## Usage 
+
+To activate **safe mode**:
+
+- Add `define( 'WP_DISABLE_PLUGINS', true );` to your wp-config.php file.
+- Append the query string `?safe_mode=true` to any url to load that post or page with no plugins running.
+- Append `&p1=plugin-slug1` to load the page with only the plugin you specify. The plugin slug is the name of the plugin's directory under `/wp-content/plugins/`.
+- Append `&p2=plugin-slug2` to load the page with a second active plugin.
+
+### Examples
+
+- View the default sample page with no plugin running `https://twenty-twentyone.local/sample-page/?safe_mode=true`
+- View the sample page with only **Query Monitor** running `https://twenty-twentyone.local/sample-page/?safe_mode=true&p1=query-monitor`
+- View the sample page with only **Query Monitor** and **Popup Maker** running `https://twenty-twentyone.local/sample-page/?safe_mode=true&p1=query-monitor&p2=popup-maker`
+
--- plugin-safe-mode.php
+++ plugin-safe-mode.php
@@ -1,14 +1,67 @@
 <?php
 /**
- * Plugin Name: Plugin Safe Mode
+ * Plugin Name: Plugin Safe Mode++
  * Plugin URI: http://core.trac.wordpress.org/ticket/25137
- * Description: Enables safe mode by preventing plugins from loading if WP_DISABLE_PLUGINS is defined true and ?safe_mode=true appended to url
- * Author: Chris Olbekson <chris@x-team.com>
+ * Description: Enables safe mode by preventing plugins from loading if
+ * WP_DISABLE_PLUGINS is defined true and ?safe_mode=true appended to url
+ * Author: caught my eye dev <mark@caughtmyeye.cc>
  * Version: 1.0
- * Author URI: http://x-team.com
+ * Author URI: https://www.caughtmyeye.dev/about/
  */
  
+/**
+ * TO DO: First iteraion. Plenty of room for refactoring.
+ */
 
 if ( defined( 'WP_DISABLE_PLUGINS' ) && WP_DISABLE_PLUGINS && isset( $_GET['safe_mode'] ) ) {
-	add_filter( 'option_active_plugins', '__return_empty_array' );
+
+	// Blindly deactivate all plugins for the post or page.
+	//add_filter( 'option_active_plugins', '__return_empty_array' );
+
+	// Deactivate all plugins except for 1 or 2 passed in as args.
+	add_filter( 'option_active_plugins', function( $plugins ){
+
+		// Master list of what plugins we can keep. Add your keepers
+		// here.
+		$plugin_list = array();
+		$plugin_list['block-visibility'] = 'block-visibility/block-visibility.php';
+		$plugin_list['code-snippets'] = 'code-snippets/code-snippets.php';
+		$plugin_list['content-control'] = 'content-control/content-control.php';
+		$plugin_list['insert-headers-and-footers'] = 'insert-headers-and-footers/ihaf.php';
+		$plugin_list['popup-maker'] = 'popup-maker/popup-maker.php';
+		$plugin_list['query-monitor'] = 'query-monitor/query-monitor.php';
+		$plugin_list['user-menus'] = 'user-menus/user-menus.php';
+
+		// The first optional plugin to keep.
+		$p1 = '';
+		$j = '';
+		if ( isset( $_GET['p1'] ) ) {
+			$p1 = $plugin_list[$_GET['p1']];
+			$j = array_search( $p1, $plugins );
+		}
+
+		// The second optional plugin to keep.
+		$p2 = '';
+		$k ='';
+		if ( isset( $_GET['p2'] ) ) {
+			$p2 = $plugin_list[$_GET['p2']];
+			$k = array_search( $p2, $plugins );
+		}
+
+		// If no args were passed in, deactivate all plugins.
+		if ( ( $j == '' ) && ( $k == '' ) ) return array();
+
+		// Else find out which plugins to keep active.
+		foreach ($plugins as $key => $value) {
+			// Deactivate all plugins except for what's passed in.
+			if ( ( ( $j !== '' ) && ( $value ==  $plugins[$j] ) ) ||
+			     ( ( $k !== '' ) && ( $value ==  $plugins[$k] ) ) ) {  
+				continue; // Skip the ones we want to keep.
+			}
+			unset( $plugins[$key] ); // If we get here, deactivate this plugin.
+		}
+
+		return $plugins;
+	
+	} );
 }
